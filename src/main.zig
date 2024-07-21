f0b0d04503b61afab9dc4e0355bd0baf1026ad15
Nico Gallinal <nicoabie@gmail.com>
2024-07-21 13:12:14 -0300

    try reading from udp

--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,42 @@
 const std = @import("std");
+const expect = std.testing.expect;
+const net = std.net;
 
-pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+const UDP_PORT = 20777;
+const LOCAL_IP = "127.0.0.1";
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
-
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-    try bw.flush(); // don't forget to flush!
+test "create a socket" {
+    const socket = try Socket.init(LOCAL_IP, UDP_PORT);
+    try expect(@TypeOf(socket.socket) == std.os.socket_t);
 }
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+const Socket = struct {
+    address: std.net.Address,
+    socket: std.posix.socket_t,
+
+    fn init(ip: []const u8, port: u16) !Socket {
+        const address = try std.net.Address.parseIp4(ip, port);
+        const socket = try std.posix.socket(std.posix.AF.INET, std.posix.SOCK.DGRAM, 0);
+        errdefer std.posix.close(socket);
+        return Socket{ .address = address, .socket = socket };
+    }
+
+    fn bind(self: *Socket) !void {
+        try std.posix.bind(self.socket, &self.address.any, self.address.getOsSockLen());
+    }
+
+    fn listen(self: *Socket) !void {
+        var buffer: [1024]u8 = undefined;
+
+        while (true) {
+            const received_bytes = try std.posix.recvfrom(self.socket, buffer[0..], 0, null, null);
+            std.debug.print("Received {d} bytes: {s}\n", .{ received_bytes, buffer[0..received_bytes] });
+        }
+    }
+};
+
+pub fn main() !void {
+    var socket = try Socket.init(LOCAL_IP, UDP_PORT);
+    try socket.bind();
+    try socket.listen();
 }
